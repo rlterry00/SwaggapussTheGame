32ef8d19de3518311c53cb48e442df9cf3c33a78
rmccoy4145 <30878588+rmccoy4145@users.noreply.github.com>
2018-06-19 13:51:55 -0400

    created main.lua
    created main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+io.stdout:setvbuf("no")
+--local class = require 'swag_classes'
+local bump = require 'bump'
+
+--BEGIN LOAD
+function love.load()
+
+end
+
+--BEGIN UPDATES
+function love.update(dt)
+	
+--END UPDATES
+end
+
+
+--DRAWING
+function love.draw()
+ 
+
+ end		
